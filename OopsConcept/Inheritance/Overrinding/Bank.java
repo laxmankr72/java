e46f07b92f167cf20dbd72676f1cf521d8d00577
Laxman <laxmantelo2003@gmail.com>
2024-06-16 23:35:55 +0530

    overriding examples

---
+++ OopsConcept/Inheritance/Overrinding/Bank.java
@@ -0,0 +1,17 @@
+package OopsConcept.Inheritance.Overrinding;
+
+ class Bank {
+    int getRateOfInterest(){
+        return 0;
+    }
+}
+class SBI extends Bank{
+    int getRateOfInterest(){return 8;}
+}
+
+class ICICI extends Bank{
+    int getRateOfInterest(){return 7;}
+}
+class AXIS extends Bank{
+    int getRateOfInterest(){return 9;}
+}
---
+++ OopsConcept/Inheritance/Overrinding/MainBank.java
@@ -0,0 +1,12 @@
+package OopsConcept.Inheritance.Overrinding;
+
+public class MainBank {
+    public static void main(String [] args){
+        SBI s=new SBI();
+        ICICI i=new ICICI();
+        AXIS a=new AXIS();
+        System.out.println("SBI Rate of Interest: "+s.getRateOfInterest());
+        System.out.println("ICICI Rate of Interest: "+i.getRateOfInterest());
+        System.out.println("AXIS Rate of Interest: "+a.getRateOfInterest());
+    }
+}
