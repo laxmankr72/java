b0d82e0591736b87afd1ac9c7e0a3ad02cf4867c
Laxman <laxmantelo2003@gmail.com>
2024-06-19 22:57:48 +0530

    Rectangular Matrix

---
+++ _2DArray/RectangularMatrix.java
@@ -0,0 +1,30 @@
+package _2DArray;
+import java.util.Scanner;
+
+public class RectangularMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Row: ");
+        int n = sc.nextInt();
+
+        System.out.println("Row: ");
+        int m = sc.nextInt();
+
+
+        int arr[][] = new int [n][m];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(arr[i][j]+1+" ");
+            }
+            System.out.println();
+        }
+    }
+}
